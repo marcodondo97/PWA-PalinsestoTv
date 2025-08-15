0a0b7a8e9be4f2444bcef6233e955811ca106b12
Marco Dondo <dondo.marco.itsv@gmail.com>
2025-08-15 10:12:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def create_app() -> Flask:
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5001"))
+    port = int(os.getenv("PORT", "8080"))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
